9655b724c45ad6713fde5e4212bcba7caa6ad359
Zihan Jin <admin@zihanjin.com>
2026-04-15 11:44:48 +1000

    fix: rename repository::repository to repository::repo to avoid inception
    fix: rename repository::repository to repository::repo to avoid inception

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,9 +1,9 @@
 mod file_repository;
-mod repository;
+mod repo;
 #[cfg(feature = "sqlite")]
 mod sqlite_repository;
 
 pub use file_repository::FileRepository;
-pub use repository::{QueryOpts, Repository};
+pub use repo::{QueryOpts, Repository};
 #[cfg(feature = "sqlite")]
 pub use sqlite_repository::SqliteRepository;
