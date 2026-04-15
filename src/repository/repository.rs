691eed26660a546a788bd33d0313fbc431fbf463
Zihan Jin <admin@zihanjin.com>
2026-04-15 11:37:06 +1000

    feat: impl Repository for Box<dyn Repository>

--- src/repository/repository.rs
+++ src/repository/repository.rs
@@ -18,3 +18,21 @@ pub trait Repository {
 
     fn end_session(&mut self, dt: DateTime<Utc>) -> Result<()>;
 }
+
+impl Repository for Box<dyn Repository> {
+    fn list(&self, opts: QueryOpts) -> Result<Vec<Entry>, StorageError> {
+        (**self).list(opts)
+    }
+
+    fn start_session(
+        &mut self,
+        desc: &str,
+        dt: chrono::DateTime<chrono::Utc>,
+    ) -> anyhow::Result<()> {
+        (**self).start_session(desc, dt)
+    }
+
+    fn end_session(&mut self, dt: chrono::DateTime<chrono::Utc>) -> anyhow::Result<()> {
+        (**self).end_session(dt)
+    }
+}
